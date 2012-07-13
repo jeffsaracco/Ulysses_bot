f29b217083e8ab3795805ae77c1a4446474eaec9
Jeff Saracco <jeffsaracco@gmail.com>
2012-07-12 21:01:38 -0400

    July 12 class

---
+++ ulysses_bot.rb
@@ -0,0 +1,31 @@
+source_text = <<EOF
+They came down the steps from Leahy's terrace prudently, Frauenzimmer: and down the shelving shore flabbily, their splayed feet sinking in the silted sand. Like me, like Algy, coming down to our mighty mother. Number one swung lourdily her midwife's bag, the other's gamp poked in the beach. From the liberties, out for the day. Mrs Florence MacCabe, relict of the late Patk MacCabe, deeply lamented, of Bride Street. One of her sisterhood lugged me squealing into life. Creation from nothing. What has she in the bag? A misbirth with a trailing navelcord, hushed in ruddy wool. The cords of all link back, strandentwining cable of all flesh. That is why mystic monks. Will you be as gods? Gaze in your omphalos. Hello! Kinch here. Put me on to Edenville. Aleph, alpha: nought, nought, one.
+Spouse and helpmate of Adam Kadmon: Heva, naked Eve. She had no navel. Gaze. Belly without blemish, bulging big, a buckler of taut vellum, no, whiteheaped corn, orient and immortal, standing from everlasting to everlasting. Womb of sin.
+Wombed in sin darkness I was too, made not begotten. By them, the man with my voice and my eyes and a ghostwoman with ashes on her breath. They clasped and sundered, did the coupler's will. From before the ages He willed me and now may not will me away or ever. A lex eterna stays about Him. Is that then the divine substance wherein Father and Son are consubstantial? Where is poor dear Arius to try conclusions? Warring his life long upon the contransmagnificandjewbangtantiality. Illstarred heresiarch' In a Greek watercloset he breathed his last: euthanasia. With beaded mitre and with crozier, stalled upon his throne, widower of a widowed see, with upstiffed omophorion, with clotted hinderparts.
+Airs romped round him, nipping and eager airs. They are coming, waves. The whitemaned seahorses, champing, brightwindbridled, the steeds of Mananaan.
+I mustn't forget his letter for the press. And after? The Ship, half twelve. By the way go easy with that money like a good young imbecile.
+Yes, I must.
+His pace slackened. Here. Am I going to aunt Sara's or not? My consubstantial father's voice. Did you see anything of your artist brother Stephen lately? No? Sure he's not down in Strasburg terrace with his aunt Sally? Couldn't he fly a bit higher than that, eh? And and and and tell us, Stephen, how is uncle Si? O, weeping God, the things I married into! De boys up in de hayloft. The drunken little costdrawer and his brother, the cornet player. Highly respectable gondoliers! And skeweyed Walter sirring his father, no less! Sir. Yes, sir. No, sir. Jesus wept: and no wonder, by Christ!
+EOF
+
+def split_text_into_pairs_of_words(text)
+  new_pair_array = []
+  text_array = text.split(" ")
+  text_array.each_with_index do |word, index|
+    new_pair_array << "#{word} #{text_array[index + 1]}"
+  end
+  new_pair_array
+end
+
+my_new_pair_array = split_text_into_pairs_of_words(source_text)
+
+def record_the_probability(text)
+  hash_of_probabilities = {}
+  text_array = text.split(" ")
+  text_array.each_with_index do |word, index|
+    temp_array = hash_of_probabilities[word]
+  end
+end
+
+
+# generate_random_text
