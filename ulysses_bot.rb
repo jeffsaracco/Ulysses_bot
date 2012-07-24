7475acbf5161c2493289c4071e58b7fff4597ced
Jeff Saracco <jeffsaracco@gmail.com>
2012-07-23 20:33:58 -0400

    Added the + method to our LitBot class

--- ulysses_bot.rb
+++ ulysses_bot.rb
@@ -10,40 +10,85 @@ class LitBot
   end
 
   def digest_file
-    source_text_words = @source_text.split(' ')
-    word_pairs_and_probabilities = {}
+    @source_text_words = @source_text.split(' ')
+    @word_pairs_and_probabilities = {}
 
-    source_text_words.each_with_index do |word, index|
-      hash_key = "#{word} #{source_text_words[index + 1]}"
-      hash_value = source_text_words[index + 2]
-      if word_pairs_and_probabilities[hash_key]
-        word_pairs_and_probabilities[hash_key] << hash_value
+    @source_text_words.each_with_index do |word, index|
+      hash_key = "#{word} #{@source_text_words[index + 1]}"
+      hash_value = @source_text_words[index + 2]
+      if @word_pairs_and_probabilities[hash_key]
+        @word_pairs_and_probabilities[hash_key] << hash_value
       else
-        word_pairs_and_probabilities[hash_key] = [hash_value]
+        @word_pairs_and_probabilities[hash_key] = [hash_value]
       end
     end
   end
 
   def speak
-    puts word_pairs_and_probabilities.first
+    output_text = generate_random_seed_pair
+    story = 0
+    while story < 35 do
+      word_pair = output_text.last(2).join(' ')
+      next_word = @word_pairs_and_probabilities[word_pair].sample unless @word_pairs_and_probabilities[word_pair].nil?
+      output_text << next_word
+
+      if next_word && (next_word.include?(".") || next_word.include?("?"))
+        story += 1
+      end
+    end
+    output_text.join(' ')
+  end
+
+  def generate_random_seed_pair
+    trial_pair = @word_pairs_and_probabilities.keys.sample
+    until trial_pair[0] == trial_pair[0].upcase do
+      trial_pair = @word_pairs_and_probabilities.keys.sample
+    end
+    trial_pair.split(' ')
+  end
+
+  def +(other_bot)
+    shiny_new_litbot = LitBot.new(@source_text + other_bot.grab_source_text)
+  end
+
+  #getter
+  def source_text
+    @source_text
+  end
+
+  def grab_source_text
+    @source_text
+  end
+
+  def set_source_text(text)
+    @source_text = text
+  end
+
+  #setter
+  def source_text=(text)
+    @source_text = text
+  end
+
+  def initialize(text="Any string")
+    set_source_text(text)
   end
 end
 
 bender = LitBot.new
 bender.eat_file("huckle.txt")
-# bender.speak_first_10_words # an example call
 bender.digest_file
-bender.speak
+# puts "Bender says, \"#{bender.speak}\""
 
 ulysses_bot = LitBot.new
 ulysses_bot.eat_file("ulysses.txt")
-# ulysses.speak_first_10_words # an example call
 ulysses_bot.digest_file
-ulysses_bot.speak
+# puts "Ulysses says, \"#{ulysses_bot.speak}\""
 
 # BONUS HW
 hybrid_bot = ulysses_bot + bender
-hybrid_bot.speak
+hybrid_bot.digest_file
+# puts hybrid_bot
+puts "Hybrid Bot says \"#{hybrid_bot.speak}\""
 
 # ulysses = File.open("ulysses.txt")
 # source_text = ulysses.read
@@ -53,13 +98,13 @@ hybrid_bot.speak
 # word_pairs_and_probabilities = {}
 
 # source_text_words.each_with_index do |word, index|
-  # hash_key = "#{word} #{source_text_words[index + 1]}"
-  # hash_value = source_text_words[index + 2]
-  # if word_pairs_and_probabilities[hash_key]
-    # word_pairs_and_probabilities[hash_key] << hash_value
-  # else
-    # word_pairs_and_probabilities[hash_key] = [hash_value]
-  # end
+# hash_key = "#{word} #{source_text_words[index + 1]}"
+# hash_value = source_text_words[index + 2]
+# if word_pairs_and_probabilities[hash_key]
+# word_pairs_and_probabilities[hash_key] << hash_value
+# else
+# word_pairs_and_probabilities[hash_key] = [hash_value]
+# end
 # end
 
 # # puts word_pairs_and_probabilities
@@ -67,13 +112,13 @@ hybrid_bot.speak
 
 # story = 0
 # while story < 35 do
-  # word_pair = output_text.last(2).join(' ')
-  # next_word = word_pairs_and_probabilities[word_pair].sample unless word_pairs_and_probabilities[word_pair].nil?
-  # output_text << next_word
+# word_pair = output_text.last(2).join(' ')
+# next_word = word_pairs_and_probabilities[word_pair].sample unless word_pairs_and_probabilities[word_pair].nil?
+# output_text << next_word
 
-  # if next_word && (next_word.include?(".") || next_word.include?("?"))
-    # story += 1
-  # end
+# if next_word && (next_word.include?(".") || next_word.include?("?"))
+# story += 1
+# end
 # end
 # puts output_text.join(' ')
 
