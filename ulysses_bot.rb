93a89903c940f607f7c8aa20913a1984804c7a85
Jeff Saracco <jeffsaracco@gmail.com>
2012-07-26 19:10:17 -0400

    Make Ulysses_Bot a web app

--- ulysses_bot.rb
+++ ulysses_bot.rb
@@ -1,3 +1,20 @@
+require 'sinatra'
+
+get '/hi' do
+  "Hello World!"
+end
+
+get '/hi-kyle' do
+  "Hello Kyle"
+end
+
+get '/ulysses-bot' do
+  ulysses_bot = LitBot.new
+  ulysses_bot.eat_file("ulysses.txt")
+  ulysses_bot.digest_file
+  "Ulysses says, \"#{ulysses_bot.speak}\""
+end
+
 class LitBot
   def eat_file(file_name)
     file = File.open(file_name)
@@ -65,58 +82,3 @@ class LitBot
     self.source_text = text
   end
 end
-
-bender = LitBot.new
-bender.eat_file("huckle.txt")
-bender.digest_file
-# puts "Bender says, \"#{bender.speak}\""
-
-ulysses_bot = LitBot.new
-ulysses_bot.eat_file("ulysses.txt")
-ulysses_bot.digest_file
-# puts "Ulysses says, \"#{ulysses_bot.speak}\""
-
-# BONUS HW
-hybrid_bot = ulysses_bot + bender
-hybrid_bot.digest_file
-# puts hybrid_bot
-puts "Hybrid Bot says \"#{hybrid_bot.speak}\""
-
-# ulysses = File.open("ulysses.txt")
-# source_text = ulysses.read
-# ulysses.close
-
-# source_text_words = source_text.split(' ')
-# word_pairs_and_probabilities = {}
-
-# source_text_words.each_with_index do |word, index|
-# hash_key = "#{word} #{source_text_words[index + 1]}"
-# hash_value = source_text_words[index + 2]
-# if word_pairs_and_probabilities[hash_key]
-# word_pairs_and_probabilities[hash_key] << hash_value
-# else
-# word_pairs_and_probabilities[hash_key] = [hash_value]
-# end
-# end
-
-# # puts word_pairs_and_probabilities
-# output_text = ['They', 'came']
-
-# story = 0
-# while story < 35 do
-# word_pair = output_text.last(2).join(' ')
-# next_word = word_pairs_and_probabilities[word_pair].sample unless word_pairs_and_probabilities[word_pair].nil?
-# output_text << next_word
-
-# if next_word && (next_word.include?(".") || next_word.include?("?"))
-# story += 1
-# end
-# end
-# puts output_text.join(' ')
-
-
-
-
-
-
-
